cd5c26bc799e6477235af3b94f552e7c79b15163
radyuk.aleksandr2 <radyuk.aleksandr2@wb.ru>
2024-09-21 20:02:38 +0300

    Changed redis settings

--- sharding-repl-cache/70_create_redis_cluster.sh
+++ sharding-repl-cache/70_create_redis_cluster.sh
@@ -4,11 +4,11 @@ echo "Creating Redis cluster..."
 docker compose exec -T redis_1 \
   bash -c 'echo "yes" | redis-cli --cluster create \
   173.17.0.2:6379 \
-  173.17.0.3:6380 \
-  173.17.0.4:6381 \
-  173.17.0.5:6382 \
-  173.17.0.6:6383 \
-  173.17.0.7:6384 \
+  173.17.0.3:6379 \
+  173.17.0.4:6379 \
+  173.17.0.5:6379 \
+  173.17.0.6:6379 \
+  173.17.0.7:6379 \
   --cluster-replicas 1'
 
 # Check cluster status
