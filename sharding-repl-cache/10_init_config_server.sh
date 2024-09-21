2859e22b820f1539b94a86ceb500310fbf1fe745
radyuk.aleksandr2 <radyuk.aleksandr2@wb.ru>
2024-09-21 16:51:10 +0300

    Task 4. Caching

---
+++ sharding-repl-cache/10_init_config_server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Initialize the config server replica set
+echo "Initializing config server replica set..."
+
+docker compose exec -T configSrv mongosh --port 27017 <<EOF
+rs.initiate(
+  {
+    _id : "config_server",
+    configsvr: true,
+    members: [
+      { _id : 0, host : "configSrv:27017" }
+    ]
+  }
+);
+EOF
